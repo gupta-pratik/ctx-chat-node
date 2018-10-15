2d37c05ed430b9243920d05a8378cf4c2c1ce3df
gupta-pratik <gprat2108@gmail.com>
2018-10-15 23:16:45 +0530

    Fixed api calls

--- functions/index.js
+++ functions/index.js
@@ -14,8 +14,9 @@ const app = dialogflow({
 });
 
 var http = require('https');
+const axios = require('axios')
 
-const host = 'api.podio.com';
+const host = 'https://api.podio.com';
 
 app.intent('Default Welcome Intent', (conv) => {
   console.log('Prateek welcomeIntent');
@@ -39,85 +40,22 @@ app.intent('Get Sign In', (conv, params, signin) => {
 
 app.intent('GetMeetingSchedule', (conv) => {
   console.log('Get Meeting Schdeule Intent Called');
-  //console.log('Conv'+JSON.parse(conv));
-  //console.log('param'+params);
-  //console.log('res'+JSON.parse(res));
-  return callAppointments(conv.user.access.token).then((output) => {
-    console.log(conv);
-    conv.json({
-      'fulfillmentText': output
-    }); // Return the results of the weather API to Dialogflow
-  })
-  // .catch(() => {
-  //   conv.json({
-  //     'fulfillmentText': `I don't know the weather but I hope it's good!`
-  //   });
-  //});
+  axios.get('https://api.podio.com/calendar/summary', {
+      headers: {
+        Authorization: 'OAuth2 ' + conv.user.access.token
+      }
+    })
+    .then(response => {
+      // If request is good...
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log('error ' + error);
+    });
+  conv.json({
+    'fulfillmentText': 'Prateek Test Response'
+  });
 })
-
-function getAppointments(authToken) {
-  var options = {
-    hostname: host,
-    path: '/calendar/summary',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'OAuth2 ' + authToken
-    }
-  };
-  console.log('Status: ' + res.statusCode);
-  var req = http.request(options, function (res) {
-    console.log('Status: ' + res.statusCode);
-    console.log('Headers: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (body) {
-      console.log('Body: ' + body);
-    });
-  });
-  req.on('error', function (e) {
-    console.log('problem with request: ' + e.message);
-  });
-}
-
-
-function callAppointments(authToken) {
-  // Create the path for the HTTP request to get the weather
-  let path = '/calendar/summary';
-  var options = {
-    hostname: host,
-    path: path,
-   // port: 8080,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'OAuth2 ' + authToken
-    }
-  };
-  console.log('Auth Token: ' + authToken);
-  return new Promise((resolve, reject) => {
-
-    console.log('API Request: ' + host + path);
-
-    // Make the HTTP request to get the weather
-    http.get(options, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => {
-        body += d;
-      }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        // Resolve the promise with the output text
-        console.log(response);
-        resolve(response);
-      });
-      res.on('error', (error) => {
-        console.log(`Error calling the weather API: ${error}`)
-        reject();
-      });
-    });
-  });
-}
 
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
